0b0ef84c017885bd5318105acb683390f760be29
Nash <72932477+Nashwalt@users.noreply.github.com>
2021-04-20 12:44:17 -0400

    Added Skills Section.

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,8 +4,30 @@ import Navbar from "./Navbar";
 
 const Skills = () => {
     return ( 
-        <Navbar/>
-        
+        <main>
+            <Navbar/>
+            <section className= "container">
+                <div className="row justify-content-between">
+                    <div className="col-lg-5 ">
+                        <h3>My Skills</h3>
+                        <p>I build creative, responsive web applications using HTML5, CSS3, JavaScript, JQuery, Bootstrap and much more...</p>
+                    </div>
+                <div className="col-lg-6">
+                    <img src="" className="logo" alt="html5 logo"/>
+                    <img src="" className="logo" alt="css3 logo"/>
+                    <img src="" className="logo img-shape" alt="bootstrap logo"/>
+                    <img src="" className="logo img-shape" alt="bulma logo"/>
+                    <img src="" className="logo" alt="html5 logo"/>
+                    <img src="" className="logo" alt="jquery logo"/>
+                    <img src="" className="logo img-shape" alt="api logo"/>
+                    <img src="" className="logo" alt="node logo"/>
+                    <img src="" className="logo" alt="mysql logo"/>
+                    <img src="" className="logo img-shape" alt="sequelize logo"/>
+                    <img src="" className="logo img-shape" alt="handlebars logo"/>
+                </div>
+                </div>
+            </section>          
+        </main>
     );
 }
  
