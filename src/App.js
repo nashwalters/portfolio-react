32bbcf1e1695f0d08fc9cd0291f724dffbc752ed
Nash <72932477+Nashwalt@users.noreply.github.com>
2021-04-20 10:55:48 -0400

    Added routing.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header'
+import "./App.css";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header"
 
 function App() {
-  return (
-    <div className="App">
-      <Header/>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+            <div className="Content">
+                <Switch>
+                    <Route exact path="/">
+                        <Header/>
+                    </Route>
+                    <Route path="/about">
+                        <About/>
+                    </Route>
+                    <Route path="/skills">
+                        <Skills/>
+                    </Route>
+                    <Route path="/projects">
+                        <Projects/>
+                    </Route>
+                    <Route path="/contact">
+                        <Contact/>
+                    </Route>
+                </Switch>
+            </div>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
