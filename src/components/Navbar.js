8d5d870e69b597d061bddf89cce76c27f1e99b0b
Nash <72932477+Nashwalt@users.noreply.github.com>
2021-04-20 18:16:48 -0400

    Made navbar sticky.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
+            <div className="collapse navbar-collapse sticky" id="navbarNav">
                 <ul className="navbar-nav mx-auto">
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/about">About</Link></li>
