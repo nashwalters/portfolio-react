4017e323e363ceaeaf10980d82c1a50bc5cf14e6
Nash <72932477+Nashwalt@users.noreply.github.com>
2021-04-20 12:24:41 -0400

    Added about me section.

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,32 @@
 import React from "react";
 import Navbar from "./Navbar";
+import { Link } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const About = () => {
     return (
-        <Navbar/>
-
- 
+        <main>
+            <Navbar />
+            <section class= "container">
+                <div class="row justify-content-between">
+                    <div class="col-lg-4 text-center">
+                        <img src="" alt=""/>
+                    </div>
+                    <div class="col-lg-7">
+                        <h3 class="text-center">About Me</h3>
+                        <h6 class="text-center text"> I'm Nashica, an Ontario based Junior Full- Stack Developer.</h6>
+                        <p>I create beautiful and functional websites. I love working in the area of responsive design.</p>
+                        <p>My unique background in the Hospitality and Retail Management gives me an edge as I know the importance of delivering a superior product with excellent customer service. </p>
+                        <p>Interested in working together? Take a look at my most recent projects. Please feel free to contact me for projects and collaborations. </p>
+                    <div class="text-center">
+                        <Link to="" target="_blank"><button class="nav-btn link link-btn">Check out my CV</button></Link>
+                        <Link to="/projects"><button class=" nav-btn link link-btn">Projects</button></Link>
+                        <Link to="/contact"><button class=" nav-btn link link-btn">Contact Me</button></Link>
+                    </div>
+                    </div>
+                </div>
+            </section>
+        </main>
     );
 }
  
