242c22b7eac78e00671dafd5db662bf349a4ef12
Nash <72932477+Nashwalt@users.noreply.github.com>
2021-04-21 11:48:58 -0400

    Deploy app.

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Contact from "./components/Contact";
 
 function App() {
     return (
-        <Router>
+        <Router basename={process.env.PUBLIC_URL}>
             <div className="App">
             <div className="Content">
                 <Switch>
--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {HashRouter as Router} from 'react-router-dom';
 
 ReactDOM.render(
-  <Router>
+  
     <App />
-  </Router>,
-  document.getElementById('root')
+ 
+  ,document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
